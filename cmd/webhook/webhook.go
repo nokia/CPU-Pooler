c045593053e42a06972c6852b5780bc3072a17ce
Timo Lindqvist <timo.lindqvist@nokia.com>
2018-11-22 13:49:52 +0200

    Add Pod manifest validation
    - Check that mandatory fields are present in cpu annotation
    - Check that pools in annotation / resources field exist
      in configuration
    - Fixes #1
    
    Signed-off-by: Timo Lindqvist <timo.lindqvist@nokia.com>

--- cmd/webhook/webhook.go
+++ cmd/webhook/webhook.go
@@ -1,13 +1,9 @@
-/*
-Based on
-
-*/
-
 package main
 
 import (
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"flag"
 	"github.com/golang/glog"
 	"github.com/nokia/CPU-Pooler/internal/types"
@@ -30,6 +26,13 @@ type patch struct {
 	Op    string          `json:"op"`
 	Path  string          `json:"path"`
 	Value json.RawMessage `json:"value"`
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func toAdmissionResponse(err error) *v1beta1.AdmissionResponse {
@@ -66,8 +69,93 @@ func cpuPoolResourcePatch(cpuAnnotation types.CPUAnnotation, pool string, contai
 
 }
 
+func patchContainer(cpuAnnotation types.CPUAnnotation, patchList []patch, i int, c *corev1.Container) ([]patch, error) {
+	var patchItem patch
+	sharedCPUTime := cpuAnnotation.ContainerSharedCPUTime(c.Name, poolConf)
+
+	glog.V(2).Infof("Adding patches")
+
+	// podinfo volumeMount
+	patchItem.Op = "add"
+	patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/volumeMounts/-"
+	patchItem.Value =
+		json.RawMessage(`{"name":"podinfo","mountPath":"/etc/podinfo","readOnly":true}`)
+	patchList = append(patchList, patchItem)
+
+	// hostbin volumeMount. Location for process starter binary
+	patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/volumeMounts/-"
+	patchItem.Value =
+		json.RawMessage(`{"name":"hostbin","mountPath":"/opt/bin","readOnly":true}`)
+	patchList = append(patchList, patchItem)
+
+	//  device plugin config volumeMount.
+	patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/volumeMounts/-"
+	patchItem.Value =
+		json.RawMessage(`{"name":"cpu-dp-config","mountPath":"/etc/cpu-dp","readOnly":true}`)
+	patchList = append(patchList, patchItem)
+
+	// Container name to env variable
+	contNameEnvPatch := `{"name":"CONTAINER_NAME","value":"` + c.Name + `" }`
+	patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/env"
+	if len(c.Env) > 0 {
+		patchItem.Path += "/-"
+	} else {
+		contNameEnvPatch = `[` + contNameEnvPatch + `]`
+	}
+	patchItem.Value = json.RawMessage(contNameEnvPatch)
+	patchList = append(patchList, patchItem)
+
+	// Overwrite entrypoint
+	patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/command"
+	patchItem.Value = json.RawMessage(`[ "/opt/bin/process-starter" ]`)
+	patchList = append(patchList, patchItem)
+
+	// Add cpu limit if container requested shared pool cpus
+	if sharedCPUTime > 0 {
+		patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/resources/limits/cpu"
+		cpuVal := `"` + strconv.Itoa(sharedCPUTime) + `m"`
+		patchItem.Value =
+			json.RawMessage(cpuVal)
+		patchList = append(patchList, patchItem)
+	}
+	// CPU pool recource request/limit from all pools of container
+	for _, pool := range cpuAnnotation.ContainerPools(c.Name) {
+		patchItem = cpuPoolResourcePatch(cpuAnnotation, pool, i, c.Name)
+		patchList = append(patchList, patchItem)
+	}
+
+	return patchList, nil
+}
+
+func validateResource(resList corev1.ResourceList) error {
+	resourceBaseNameLen := len(poolConf.ResourceBaseName)
+	for key := range resList {
+		compLen := min(len(key), resourceBaseNameLen)
+		if string(key)[:compLen] == poolConf.ResourceBaseName {
+			poolName := string(key)[resourceBaseNameLen+1:]
+			if _, found := poolConf.Pools[poolName]; !found {
+				return errors.New(poolName + " not found from pool config")
+			}
+		}
+	}
+	return nil
+}
+
+func validateContainerResourceSpec(c *corev1.Container) error {
+	if err := validateResource(c.Resources.Limits); err != nil {
+		return err
+	}
+	if err := validateResource(c.Resources.Requests); err != nil {
+		return err
+	}
+	return nil
+}
+
 func mutatePods(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 	glog.V(2).Info("mutating pods")
+	var err error
+	var patchList []patch
+
 	podResource := metav1.GroupVersionResource{Group: "", Version: "v1", Resource: "pods"}
 	if ar.Request.Resource != podResource {
 		glog.Errorf("expect resource to be %s", podResource)
@@ -77,7 +165,7 @@ func mutatePods(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 	raw := ar.Request.Object.Raw
 	pod := corev1.Pod{}
 	deserializer := codecs.UniversalDeserializer()
-	if _, _, err := deserializer.Decode(raw, nil, &pod); err != nil {
+	if _, _, err = deserializer.Decode(raw, nil, &pod); err != nil {
 		glog.Error(err)
 		return toAdmissionResponse(err)
 	}
@@ -88,106 +176,66 @@ func mutatePods(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 
 	reviewResponse.Allowed = true
 
+	// Mutate containers if cpu annotation exists.
 	if podAnnotation, exists := pod.ObjectMeta.Annotations[annotationName]; exists {
 		glog.V(2).Infof("mutatePod : Annotation %v", podAnnotation)
-		var patchList []patch
-		var patchItem patch
-
 		cpuAnnotation := types.CPUAnnotation{}
-		err := cpuAnnotation.Decode([]byte(podAnnotation))
 
+		err = cpuAnnotation.Decode([]byte(podAnnotation), poolConf)
 		if err != nil {
 			glog.Errorf("Failed to decode pod annotation %v", err)
 			return toAdmissionResponse(err)
 		}
-
 		containersToPatch := cpuAnnotation.Containers()
-
 		glog.V(2).Infof("Patch containers %v", containersToPatch)
 
+		// Patch container if needed. If no annotation for container, validate
+		// possible CPU-Pooler resource requests/limits
 		for i, c := range pod.Spec.Containers {
-			if !isContainerPatchNeeded(c.Name, containersToPatch) {
-				continue
-			}
-			sharedCPUTime := cpuAnnotation.ContainerSharedCPUTime(c.Name, poolConf)
-
-			glog.V(2).Infof("Adding patches")
-
-			// podinfo volumeMount
-			patchItem.Op = "add"
-			patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/volumeMounts/-"
-			patchItem.Value =
-				json.RawMessage(`{"name":"podinfo","mountPath":"/etc/podinfo","readOnly":true}`)
-			patchList = append(patchList, patchItem)
-
-			// hostbin volumeMount. Location for process starter binary
-			patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/volumeMounts/-"
-			patchItem.Value =
-				json.RawMessage(`{"name":"hostbin","mountPath":"/opt/bin","readOnly":true}`)
-			patchList = append(patchList, patchItem)
-
-			//  device plugin config volumeMount.
-			patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/volumeMounts/-"
-			patchItem.Value =
-				json.RawMessage(`{"name":"cpu-dp-config","mountPath":"/etc/cpu-dp","readOnly":true}`)
-			patchList = append(patchList, patchItem)
-
-			// Container name to env variable
-			contNameEnvPatch := `{"name":"CONTAINER_NAME","value":"` + c.Name + `" }`
-			patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/env"
-			if len(c.Env) > 0 {
-				patchItem.Path += "/-"
-			} else {
-				contNameEnvPatch = `[` + contNameEnvPatch + `]`
-			}
-			patchItem.Value = json.RawMessage(contNameEnvPatch)
-			patchList = append(patchList, patchItem)
-
-			// Overwrite entrypoint
-			patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/command"
-			patchItem.Value = json.RawMessage(`[ "/opt/bin/process-starter" ]`)
-			patchList = append(patchList, patchItem)
-
-			// Add cpu limit if container requested shared pool cpus
-			if sharedCPUTime > 0 {
-				patchItem.Path = "/spec/containers/" + strconv.Itoa(i) + "/resources/limits/cpu"
-				cpuVal := `"` + strconv.Itoa(sharedCPUTime) + `m"`
-				patchItem.Value =
-					json.RawMessage(cpuVal)
-				patchList = append(patchList, patchItem)
-			}
-			// CPU pool recource request/limit from all pools of container
-			for _, pool := range cpuAnnotation.ContainerPools(c.Name) {
-				patchItem = cpuPoolResourcePatch(cpuAnnotation, pool, i, c.Name)
-				patchList = append(patchList, patchItem)
+			if isContainerPatchNeeded(c.Name, containersToPatch) {
+				patchList, err = patchContainer(cpuAnnotation, patchList, i, &c)
+				if err != nil {
+					return toAdmissionResponse(err)
+				}
 			}
 		}
-
-		if len(patchList) > 0 {
-			// podinfo volume
-			patchItem.Path = "/spec/volumes/-"
-			patchItem.Value = json.RawMessage(`{"name":"podinfo","downwardAPI": { "items": [ { "path" : "annotations","fieldRef":{ "fieldPath": "metadata.annotations"} } ] } }`)
-			patchList = append(patchList, patchItem)
-			// hostbin volume
-			patchItem.Path = "/spec/volumes/-"
-			patchItem.Value = json.RawMessage(`{"name":"hostbin","hostPath":{ "path":"/opt/bin"} }`)
-			patchList = append(patchList, patchItem)
-
-			// cpu dp configmap volume
-			patchItem.Path = "/spec/volumes/-"
-			patchItem.Value = json.RawMessage(`{"name":"cpu-dp-config","configMap":{ "name":"cpu-dp-configmap"} }`)
-			patchList = append(patchList, patchItem)
-
-			patch, err := json.Marshal(patchList)
-			if err != nil {
-				glog.Errorf("Patch marshall error %v:%v", patchList, err)
-				reviewResponse.Allowed = false
-			}
-			reviewResponse.Patch = []byte(patch)
-			pt := v1beta1.PatchTypeJSONPatch
-			reviewResponse.PatchType = &pt
+	}
+	// Validate CPU-Pooler resource requests/limits for containers
+	for _, c := range pod.Spec.Containers {
+		err = validateContainerResourceSpec(&c)
+		if err != nil {
+			return toAdmissionResponse(err)
 		}
+	}
 
+	// Add volumes if any container was patched
+	if len(patchList) > 0 {
+		var patchItem patch
+		patchItem.Op = "add"
+
+		// podinfo volume
+		patchItem.Path = "/spec/volumes/-"
+		patchItem.Value = json.RawMessage(`{"name":"podinfo","downwardAPI": { "items": [ { "path" : "annotations","fieldRef":{ "fieldPath": "metadata.annotations"} } ] } }`)
+		patchList = append(patchList, patchItem)
+		// hostbin volume
+		patchItem.Path = "/spec/volumes/-"
+		patchItem.Value = json.RawMessage(`{"name":"hostbin","hostPath":{ "path":"/opt/bin"} }`)
+		patchList = append(patchList, patchItem)
+
+		// cpu dp configmap volume
+		patchItem.Path = "/spec/volumes/-"
+		patchItem.Value = json.RawMessage(`{"name":"cpu-dp-config","configMap":{ "name":"cpu-dp-configmap"} }`)
+		patchList = append(patchList, patchItem)
+
+		patch, err := json.Marshal(patchList)
+		if err != nil {
+			glog.Errorf("Patch marshall error %v:%v", patchList, err)
+			reviewResponse.Allowed = false
+			return toAdmissionResponse(err)
+		}
+		reviewResponse.Patch = []byte(patch)
+		pt := v1beta1.PatchTypeJSONPatch
+		reviewResponse.PatchType = &pt
 	}
 	return &reviewResponse
 }
