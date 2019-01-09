db408b60705e442ab4027db923016044add3239e
Timo Lindqvist <timo.lindqvist@nokia.com>
2019-01-09 13:02:28 +0200

    Fix testdata.
    - Do some cleaning also

--- pkg/k8sclient/k8sclient.go
+++ pkg/k8sclient/k8sclient.go
@@ -1,7 +1,6 @@
 package k8sclient
 
 import (
-	"fmt"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -27,24 +26,5 @@ func GetNodeLabels() map[string]string {
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Printf("NODES labels: %v\n", nodes.ObjectMeta.Labels)
 	return nodes.ObjectMeta.Labels
-}
-
-func PrintPod(podName string, podNameSpace string) {
-	// creates the in-cluster config
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
-	// creates the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-	pod, err := clientset.CoreV1().Pods(podNameSpace).Get(podName, metav1.GetOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("NODES labels: %v\n", pod)
 }
