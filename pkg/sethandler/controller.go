cca75d8339a2bf1ab47fcf86cc7200095233ac71
Levente Kale <levente.kale@nokia.com>
2019-01-10 17:41:47 +0100

    When allocating exlusive CPUs, setter will now check if the container will use the services of the pinner process, or not. If yes, the container's cpuset will include the ID of the core configured to host the threads of the Pinner itself. Currently it is hard-coded to be the first core. The real containerID of a container is also now searched from the PodSpec, based on the container name. This info is the basis of locating the cgroupfs hierarchy of the container.

--- pkg/sethandler/controller.go
+++ pkg/sethandler/controller.go
@@ -1,19 +1,25 @@
 package sethandler
 
 import (
+  "encoding/json"
   "errors"
   "log"
   "reflect"
   "strconv"
   "strings"
   "time"
+  origType "github.com/Levovar/CPU-Pooler/internal/types"
   "github.com/Levovar/CPU-Pooler/pkg/types"
   "github.com/intel/multus-cni/checkpoint"
   "k8s.io/api/core/v1"
-  k8stypes "k8s.io/apimachinery/pkg/types"
   "k8s.io/client-go/informers"
   "k8s.io/client-go/kubernetes"
-  "k8s.io/client-go/tools/cache"  
+  "k8s.io/client-go/tools/cache"
+)
+
+var (
+  dedicatedPinnerCoreId = 0
+  processConfigKey = "nokia.k8s.io/cpus"
 )
 
 type SetHandler struct {
@@ -44,34 +50,35 @@ func (setHandler *SetHandler) CreateController() cache.Controller {
 
 func (setHandler *SetHandler) podAdded(pod v1.Pod) {
   for _, container := range pod.Spec.Containers {
-    cpuset,err := setHandler.determineCorrectCpuset(pod.ObjectMeta.UID,container)
+    cpuset,err := setHandler.determineCorrectCpuset(pod,container)
     if err!=nil {
       log.Println("ERROR: Cpuset for the containers of Pod:" + string(pod.ObjectMeta.UID) + " could not be re-adjusted, because:" + err.Error())
-      return
+      continue
     }
-    applyCpuset(cpuset)      
+    containerId := determineCid(pod.Status,container.Name)
+    setHandler.applyCpusetToContainer(containerId,cpuset)
   }
   return
 }
 
-func (setHandler *SetHandler) determineCorrectCpuset(podUuid k8stypes.UID, container v1.Container) ([]int,error) {
+func (setHandler *SetHandler) determineCorrectCpuset(pod v1.Pod, container v1.Container) ([]int,error) {
   for resourceName, _ := range container.Resources.Requests {
     resNameAsString := string(resourceName)
     if strings.Contains(resNameAsString, setHandler.poolConfig.DeviceBaseName) && strings.Contains(resNameAsString, "shared") {
       return setHandler.poolConfig.Shared,nil
     } else if strings.Contains(resNameAsString, setHandler.poolConfig.DeviceBaseName) && strings.Contains(resNameAsString, "exclusive") {
-      return setHandler.getListOfAllocatedExclusiveCpus(resNameAsString,podUuid,container)
+      return setHandler.getListOfAllocatedExclusiveCpus(resNameAsString,pod,container)
     }
   }
   return setHandler.poolConfig.Default,nil
 }
 
-func (setHandler *SetHandler) getListOfAllocatedExclusiveCpus(exclusivePoolName string, podUuid k8stypes.UID, container v1.Container) ([]int,error) {
+func (setHandler *SetHandler) getListOfAllocatedExclusiveCpus(exclusivePoolName string, pod v1.Pod, container v1.Container) ([]int,error) {
   checkpoint, err := checkpoint.GetCheckpoint()
   if err != nil {
     return nil, errors.New("Kubelet checkpoint file could not be accessed because:"+err.Error())
   }
-  podIdStr := string(podUuid)
+  podIdStr := string(pod.ObjectMeta.UID)
   devices, err := checkpoint.GetComputeDeviceMap(podIdStr)
   if err != nil {
     return nil, errors.New("List of assigned devices could not be read from checkpoint file for Pod: "+ podIdStr +" because:"+err.Error())
@@ -82,17 +89,51 @@ func (setHandler *SetHandler) getListOfAllocatedExclusiveCpus(exclusivePoolName 
     return nil,nil
   }
   var finalCpuSet []int
+  var doesSetContainPinnerCore bool
   for _, deviceId := range exclusiveCpus.DeviceIDs {
     idAsInt, err := strconv.Atoi(deviceId)
     if err!=nil {
       return nil, errors.New("Device ID: " + deviceId + " for Container: " + container.Name + " in Pod: " + podIdStr + " is invalid")
     }
     finalCpuSet = append(finalCpuSet, idAsInt)
-  } 
+    if idAsInt == dedicatedPinnerCoreId {
+      doesSetContainPinnerCore = true
+    }
+  }
+  if isPinnerUsedByContainer(pod,container.Name) && !doesSetContainPinnerCore {
+    finalCpuSet = append(finalCpuSet, dedicatedPinnerCoreId)
+  }
   return finalCpuSet,nil
 }
 
-func applyCpuset(cpuset []int) {
+func determineCid(podStatus v1.PodStatus, containerName string) string {
+  for _,containerStatus := range podStatus.ContainerStatuses {
+    if containerStatus.Name == containerName {
+      return containerStatus.ContainerID
+    } 
+  }
+  return ""
+}
+
+func isPinnerUsedByContainer(pod v1.Pod,containerName string) bool {
+  for key, value := range pod.ObjectMeta.Annotations {
+    if strings.Contains(key,processConfigKey) {
+      var processConfig origType.CPUAnnotation
+      err := json.Unmarshal([]byte(value), &processConfig)
+      if err != nil {
+        return false
+      }
+      containerConfig := processConfig.ContainerPools(containerName)
+      //1: Container is asking exclusive CPUs + 2: Container has a pool configured in the annotation = Pinner will be used
+      if len(containerConfig) > 0 {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+func (setHandler *SetHandler) applyCpusetToContainer(containerId string, cpuset []int) {
   if cpuset == nil {
     //Nothing to set. We will leave the container running on the Kubernetes default cpuset
     return
