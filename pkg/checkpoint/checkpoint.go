015d6923129ddca04013449f2b9c4b09ba87632f
Levovar <levente.kale@nokia.com>
2021-05-04 12:47:48 +0200

    Fixing #65 - K8s 1.21 checkpoint file incompatibility issue

---
+++ pkg/checkpoint/checkpoint.go
@@ -0,0 +1,52 @@
+package checkpoint
+
+// CheckpointPodDevicesEntry is representing Pod specific deviceID allocations from kubelet checkpoint file structure - valid until K8s 1.20
+// TODO: REMOVE THIS TPYE AFTER 1.20 SUPPORT IS DROPPED
+type CheckpointPodDevicesEntry struct {
+	PodUID        string
+	ContainerName string
+	ResourceName  string
+	DeviceIDs     []string
+}
+
+// NewCheckpointPodDevicesEntry is representing Pod specific deviceID allocations from kubelet checkpoint file structure - valid from K8s 1.21 onward
+// Reference: https://github.com/kubernetes/kubernetes/commit/a8b8995ef241e93e9486d475126450f33f24ef4e
+type NewCheckpointPodDevicesEntry struct {
+	PodUID        string
+	ContainerName string
+	ResourceName  string
+	DeviceIDs     map[int64][]string
+}
+
+// CheckpointFile is representing the kubelet checkpoint file structure with only relevant fields - valid until K8s 1.20
+// TODO: REMOVE THIS TPYE AFTER 1.20 SUPPORT IS DROPPED
+type CheckpointFile struct {
+	Data struct {
+		PodDeviceEntries []CheckpointPodDevicesEntry
+	}
+}
+
+// NewCheckpointFile is representing the kubelet checkpoint file structure with only relevant fields - valid from K8s 1.21 onward
+// Reference: https://github.com/kubernetes/kubernetes/commit/a8b8995ef241e93e9486d475126450f33f24ef4e
+type NewCheckpointFile struct {
+	Data struct {
+		PodDeviceEntries []NewCheckpointPodDevicesEntry
+	}
+}
+
+// TranslateNewCheckpointToOld downgrades from an 1.21+ checkpoint file representation to the old format
+// It simply merges all the NUMA specififc DeviceID string slices into one big slice
+// Enables code re-use without needing to modify the business logic of an Operator needing to simultaneously support pre, and post 1.21 K8s versions
+func TranslateNewCheckpointToOld(newFile NewCheckpointFile) CheckpointFile {
+	var oldFile CheckpointFile
+	oldFile.Data.PodDeviceEntries = make([]CheckpointPodDevicesEntry, len(newFile.Data.PodDeviceEntries))
+	for entryID, podDeviceEntry := range newFile.Data.PodDeviceEntries {
+		oldFile.Data.PodDeviceEntries[entryID].PodUID = podDeviceEntry.PodUID
+		oldFile.Data.PodDeviceEntries[entryID].ContainerName = podDeviceEntry.ContainerName
+		oldFile.Data.PodDeviceEntries[entryID].ResourceName = podDeviceEntry.ResourceName
+		for _, devicesPerNUMA := range podDeviceEntry.DeviceIDs {
+			oldFile.Data.PodDeviceEntries[entryID].DeviceIDs = append(oldFile.Data.PodDeviceEntries[entryID].DeviceIDs, devicesPerNUMA...)
+		}
+	}
+	return oldFile
+}
--- test/utils/tempfilesys.go
+++ test/utils/tempfilesys.go
@@ -103,23 +103,23 @@ func CreateCheckpointFile() error {
 		chkpntPath = "/var/lib/kubelet/device-plugins"
 		fileName   = "kubelet_internal_checkpoint"
 		content    = `{"Data":{"PodDeviceEntries":[
-			{"PodUID":"pod0002","ContainerName":"cont_exc","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["3","4"]},
-			{"PodUID":"pod0003","ContainerName":"cont_excl1","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["3","4"]},
-			{"PodUID":"pod0003","ContainerName":"cont_excl2","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["5","6","7"]},
-			{"PodUID":"pod0004","ContainerName":"cont_exclusive","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["8"]},
-			{"PodUID":"pod0005","ContainerName":"cont_exclusive","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["3"]},
-			{"PodUID":"pod0008","ContainerName":"cont_exc_pin1","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["12","13"]},
-			{"PodUID":"pod0008","ContainerName":"cont_exc_pin2","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["14","16"]},
-			{"PodUID":"pod0009","ContainerName":"cont_exc_pin","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["6"]},
-			{"PodUID":"pod0010","ContainerName":"cont_pin_exc_shared","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["16"]},
+			{"PodUID":"pod0002","ContainerName":"cont_exc","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["3","4"]}},
+			{"PodUID":"pod0003","ContainerName":"cont_excl1","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["3","4"]}},
+			{"PodUID":"pod0003","ContainerName":"cont_excl2","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["5","6","7"]}},
+			{"PodUID":"pod0004","ContainerName":"cont_exclusive","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["8"]}},
+			{"PodUID":"pod0005","ContainerName":"cont_exclusive","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["3"]}},
+			{"PodUID":"pod0008","ContainerName":"cont_exc_pin1","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["12","13"]}},
+			{"PodUID":"pod0008","ContainerName":"cont_exc_pin2","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["14","16"]}},
+			{"PodUID":"pod0009","ContainerName":"cont_exc_pin","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["6"]}},
+			{"PodUID":"pod0010","ContainerName":"cont_pin_exc_shared","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["16"]}},
 			{"PodUID":"pod0014","ContainerName":"chckpnt_no_device","ResourceName":"nokia.k8s.io/exclusive_caas"},
-			{"PodUID":"pod0015","ContainerName":"chckpnt_no_res","DeviceIDs":["4"]},
+			{"PodUID":"pod0015","ContainerName":"chckpnt_no_res","DeviceIDs":{"0":["4"]}},
 			{"PodUID":"pod0016","ContainerName":"chckpnt_no_device_no_res"},
-			{"PodUID":"pod0019","ContainerName":"bad_deviceID_format","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["a","b","c"]},
-			{"PodUID":"pod0020","ContainerName":"no_cpuset_file","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["3","4","7"]},
-			{"PodUID":"pod0021","ContainerName":"naming_mismatch","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["3","4","7"]},
-      {"PodUID":"pod0023","ContainerName":"cont_exc_ht","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":["22","35"]}],
-			"RegisteredDevices":{"nokia.k8s.io/default":["0-2"],"nokia.k8s.io/exclusive_caas":["3","4","5","6","7","8","12","13","14","16"],"nokia.k8s.io/shared_caas":["5889","74","97","324","383","951"]}},
+			{"PodUID":"pod0019","ContainerName":"bad_deviceID_format","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["a","b","c"]}},
+			{"PodUID":"pod0020","ContainerName":"no_cpuset_file","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["3","4","7"]}},
+			{"PodUID":"pod0021","ContainerName":"naming_mismatch","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["3","4","7"]}},
+      {"PodUID":"pod0023","ContainerName":"cont_exc_ht","ResourceName":"nokia.k8s.io/exclusive_caas","DeviceIDs":{"0":["22","35"]}}],
+			"RegisteredDevices":{"nokia.k8s.io/default":["0-2"],"nokia.k8s.io/exclusive_caas":["3","4","5","6","7","8","12","13","14","16","22","35"],"nokia.k8s.io/shared_caas":["5889","74","97","324","383","951"]}},
 			"Checksum":403603645}`
 	)
 	err := exec.Command("sudo", "mkdir", "-p", chkpntPath).Run()
